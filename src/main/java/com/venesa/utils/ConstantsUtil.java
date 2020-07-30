a84e0f6a20ba0f083f836ed539bc569658b3f81d
Thuan Le Minh (HOHN-IT) <thuanlm@deaura.com.vn>
2020-07-30 13:51:23 +0700

    create api create customer and add feature validator

--- src/main/java/com/venesa/utils/ConstantsUtil.java
+++ src/main/java/com/venesa/utils/ConstantsUtil.java
@@ -4,18 +4,18 @@ public class ConstantsUtil {
 	public static final String OK = "OK";
 	public static final String UNAUTHORIZED = "UNAUTHORIZED";
 	public static final String FORBIDDEN = "FORBIDDEN";
-	
+
 	public static final String HTTP_POST = "POST";
 	public static final String HTTP_PUT = "PUT";
 	public static final String HTTP_GET = "GET";
 	public static final String HTTP_DELETE = "DELETE";
-	
-	
+
 	public static final String ERR_UNAUTHORIZED = "Token is invalid or is expired. Please login again.";
 	public static final String ERR_FORBIDDEM = "Access is denied.";
-	
-	
+
 	public static final String SUCCSESS = "success";
 	public static final String LOGGIN = "Tai khoan dang duoc dang nhap";
-	
+
+	public static final String REGEX_NUMBER_PHONE = "(0/91)?[7-9][0-9]{9}";
+
 }
