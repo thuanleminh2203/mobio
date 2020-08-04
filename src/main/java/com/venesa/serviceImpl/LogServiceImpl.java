f50be1f698cbbd41b47cb91027c51e0aca4e4e8a
Thuan Le Minh (HOHN-IT) <thuanlm@deaura.com.vn>
2020-08-04 11:55:17 +0700

    add hazelcast

--- src/main/java/com/venesa/serviceImpl/LogServiceImpl.java
+++ src/main/java/com/venesa/serviceImpl/LogServiceImpl.java
@@ -3,6 +3,7 @@ package com.venesa.serviceImpl;
 import com.venesa.entity.LogEntity;
 import com.venesa.repository.LogRepository;
 import com.venesa.service.LogService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
@@ -25,12 +26,19 @@ public class LogServiceImpl implements LogService {
     @Cacheable(cacheNames = { "longCache" })
     public List<LogEntity> getAll() {
         System.out.println("=====getAlll Log============");
+//        logRepository.findAll().stream().forEach(k -> {
+//            JSONObject jsonObject = new JSONObject(k.getResponseBody());
+//            System.out.println("===== jsonObject ====" + jsonObject.toString().replace("\"",""));
+//            String body = k.getResponseBody().replaceAll(".*\".*", "\\\"");
+//            System.out.println("======= body ====" + body);
+//        });
         return logRepository.findAll();
     }
 
     @Override
     @CacheEvict(value = "longCache" , allEntries = true)
     public void deleteById(long id) {
+        System.out.println("=====delete by id =======");
         logRepository.deleteById(id);
     }
 }
