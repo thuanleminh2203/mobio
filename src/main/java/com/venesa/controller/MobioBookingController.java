3446ec181e392a6bb2b6a2d429498974a3575653
Son Van Sy Hai (HOHN-IT) <sonvsh@deaura.com.vn>
2020-08-16 21:15:30 +0700

    son fix gateway ngay 16 thang 8

--- src/main/java/com/venesa/controller/MobioBookingController.java
+++ src/main/java/com/venesa/controller/MobioBookingController.java
@@ -2,6 +2,8 @@ package com.venesa.controller;
 //
 
 import com.venesa.common.DTO.ResponseData;
+import com.venesa.common.DTO.crm.request.CRMBookingBase;
+import com.venesa.common.DTO.crm.response.CRMBookingUpdateRes;
 import com.venesa.common.DTO.mobio.request.BookingBase;
 import com.venesa.common.DTO.mobio.request.BookingDTO;
 import com.venesa.common.DTO.mobio.response.MobioResponse;
@@ -20,54 +22,46 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin
 @AllArgsConstructor
-//@RequestMapping(ConstantsUtil.URL_GATEWAY+"booking")
-@RequestMapping("/mobio")
+@RequestMapping(ConstantsUtil.URL_GATEWAY)
 public class MobioBookingController {
     private final WrapperResponseData wrapperResponse;
     private final WebClientComponent webClientComponent;
     private final EnvironmentConfig environmentConfig;
 
-    @PutMapping("/updateAppointment")
-    public ResponseEntity<?> update(@RequestBody BookingBase rq, BindingResult result) {
-        String url = environmentConfig.getSourceUpdateBooking();
-        System.out.println("ID = " + rq.getBookingCode());
-        System.out.println("APP_TIME = " + rq.getAppointmentTime());
-        System.out.println("CA HEN = " + rq.getWorkShiftId());
+    @PutMapping("updateAppointment")
+    public ResponseEntity<?> update(@RequestBody CRMBookingBase rq, BindingResult result) {
+        String url = environmentConfig.getSourceCRMUpdateBooking();
         if (result.hasErrors()) {
             return wrapperResponse.error(
                     new ResponseData<>(ConstantsUtil.ERROR, result.getFieldError().getDefaultMessage(), null),
                     HttpStatus.BAD_REQUEST);
         }
         try {
-            BookingBase response = webClientComponent.callOuterService(new ParameterizedTypeReference<BookingBase>() {
+            BookingBase response = webClientComponent.callInternalService(new ParameterizedTypeReference<BookingBase>() {
             }, rq, HttpMethod.PUT, url, BookingBase.class);
-
-            return wrapperResponse.success(new ResponseData<>(ConstantsUtil.SUCCSESS, ConstantsUtil.SUCCSESS_MESS, response));
+            CRMBookingUpdateRes bookingUpdateRes = new CRMBookingUpdateRes(response.getBookingCode());
+            return wrapperResponse.success(new ResponseData<>(ConstantsUtil.SUCCSESS, ConstantsUtil.SUCCSESS_MESS, bookingUpdateRes));
         } catch (Exception e) {
             return wrapperResponse.error(new ResponseData<>(ConstantsUtil.ERROR, e.getMessage(), null), HttpStatus.BAD_REQUEST);
         }
-
-
     }
 
-    @PostMapping
-    public ResponseEntity<?> create(@RequestBody BookingDTO rq, BindingResult result) {
-        String url = environmentConfig.getSourceBooking();
-
-        if (result.hasErrors()) {
-            return wrapperResponse.error(
-                    new ResponseData<>(ConstantsUtil.ERROR, result.getFieldError().getDefaultMessage(), null),
-                    HttpStatus.BAD_REQUEST);
-        }
-        try {
-            MobioResponse response = webClientComponent.callOuterService(new ParameterizedTypeReference<MobioResponse>() {
-            }, rq, HttpMethod.POST, url, MobioResponse.class);
-
-            return wrapperResponse.success(new ResponseData<>(ConstantsUtil.SUCCSESS, ConstantsUtil.SUCCSESS_MESS, response));
-        } catch (Exception e) {
-            return wrapperResponse.error(new ResponseData<>(ConstantsUtil.ERROR, e.getMessage(), null), HttpStatus.BAD_REQUEST);
-        }
-
-
-    }
+//    @PostMapping
+//    public ResponseEntity<?> create(@RequestBody BookingDTO rq, BindingResult result) {
+//        String url = environmentConfig.getSourceBooking();
+//
+//        if (result.hasErrors()) {
+//            return wrapperResponse.error(
+//                    new ResponseData<>(ConstantsUtil.ERROR, result.getFieldError().getDefaultMessage(), null),
+//                    HttpStatus.BAD_REQUEST);
+//        }
+//        try {
+//            MobioResponse response = webClientComponent.callOuterService(new ParameterizedTypeReference<MobioResponse>() {
+//            }, rq, HttpMethod.POST, url, MobioResponse.class);
+//
+//            return wrapperResponse.success(new ResponseData<>(ConstantsUtil.SUCCSESS, ConstantsUtil.SUCCSESS_MESS, response));
+//        } catch (Exception e) {
+//            return wrapperResponse.error(new ResponseData<>(ConstantsUtil.ERROR, e.getMessage(), null), HttpStatus.BAD_REQUEST);
+//        }
+//    }
 }
